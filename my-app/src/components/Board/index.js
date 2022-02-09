dc5c595ae3a905344529a0eb77f53ee5b1e07f11
waynethrower <wayne.thrower@hotmail.co.uk>
2022-02-09 12:32:31 +0000

    First draft

---
+++ my-app/src/components/Board/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+//styling
+import "../App/App.css";
+
+//components
+import Square from "../Square";
+
+function Board({ board, move }) {
+  return (
+    <div className="Board">
+      <div className="parent">
+        <div className="div1">
+          {" "}
+          <Square />
+        </div>
+        <div className="div2"> 1</div>
+        <div className="div3"> 2</div>
+        <div className="div4"> 3</div>
+        <div className="div5"> 4</div>
+        <div className="div6"> 5</div>
+        <div className="div7"> 6</div>
+        <div className="div8"> 7</div>
+        <div className="div9"> 8</div>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
---
+++ my-app/src/components/Square/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Square({ move, value }) {
+  return <div>{/* need to display the value */}</div>;
+}
+
+export default Square;
